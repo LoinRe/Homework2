5d59ac4086bbc31b4099c848321f3dcd6ae3c614
LoinRe <winard2@mail.ru>
2024-07-21 15:54:22 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,69 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 1 задание
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        // 2 задание
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        // 3 задание
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        // 4 задание
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        // 5 задание
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        // 6 задание
+        var fighterWeight1 = 78.2;
+        var fighterWeight2 = 82.7;
+        var sumWeight = fighterWeight1 + fighterWeight2;
+        System.out.println(sumWeight);
+        var weightDifference = fighterWeight2 - fighterWeight1;
+        System.out.println(weightDifference);
+
+        // 7 задание
+        var num1 = 78.2;
+        var num2 = 113.9;
+        var mod = num2 % num1;
+        System.out.println(mod);
+
+        // 8 задание
+        var hours = 640;
+        float coworkerWorkTime = 8;
+        var numOfCoworkers = hours / coworkerWorkTime;
+        System.out.println("Всего работников в компании - " + numOfCoworkers + " человек.");
+        numOfCoworkers = numOfCoworkers + 94;
+        coworkerWorkTime = hours / numOfCoworkers;
+        System.out.println("Если в компании работает " + numOfCoworkers + " человек, то всего " + coworkerWorkTime + " часов работы может быть поделено между сотрудниками.");
     }
 }
